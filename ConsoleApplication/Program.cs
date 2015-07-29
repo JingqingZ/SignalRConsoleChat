5e24e7932cd779bfc831c77675e57d7f5f6707cc
unknown <zhangjqsmiling@gmail.com>
2015-07-29 16:34:00 +0100

    chat

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace SignalRConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var connection = new HubConnection("http://127.0.0.1:8088/");
+            var myHub = connection.CreateHubProxy("ChatHub");
+
+            connection.Start().ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    Console.WriteLine("There was an error opening the connection:{0}",
+                                      task.Exception.GetBaseException());
+                }
+                else
+                {
+                    Console.WriteLine("Connected");
+                }
+
+            }).Wait();
+
+            myHub.On<string>("addMessage", param =>
+            {
+                Console.WriteLine(param);
+            });
+
+
+            Console.Write("Your name: ");
+            string name = Console.ReadLine();
+
+            myHub.Invoke<string>("Chat", "Online: " + name).Wait();
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                myHub.Invoke<string>("Chat", name + ":" + message).Wait();
+                if (message == "quit")
+                    break;
+            }
+            connection.Stop();
+        }
+    }
+}
---
+++ SignalRConsole/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Hosting;
+using Owin;
+using SignalRConsole;
+
+//[assembly: OwinStartup(typeof(Startup))]
+
+namespace SignalRConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://*:8088";
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+
+    [HubName("ChatHub")]
+    public class MyHub : Hub
+    {
+        public void Chat(string param)
+        {
+            Console.WriteLine(param);
+            Clients.All.addMessage(param);
+        }
+    }
+}
